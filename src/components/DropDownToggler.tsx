463c4fad1d295569458aacffa695bc47cf7e05a2
Fox Danger Piacenti <fox@opencraft.com>
2026-06-16 10:48:04 -0500

    fix: build

--- src/components/DropDownToggler.tsx
+++ src/components/DropDownToggler.tsx
@@ -2,12 +2,11 @@ import React, { useContext } from 'react';
 import Avatar from '@edx/frontend-component-header/dist/Avatar';
 import { DesktopUserMenuTogglePropTypes } from '@edx/frontend-component-header/dist/desktop-header/DesktopUserMenuToggle';
 import { AppContext } from '@edx/frontend-platform/react';
-import { AppContextInterface } from '../types';
 
-export const DropDownToggler = ({ avatar }): { avatar: string } => {
-  const { authenticatedUser } = useContext<AppContextInterface>(AppContext);
+export const DropDownToggler = ({ avatar }) => {
+  const { authenticatedUser } = useContext(AppContext);
   return (
-    <Avatar size="1.5em" src={avatar} alt={authenticatedUser.username} />
+    <Avatar size="1.5em" src={avatar} alt={authenticatedUser?.username || 'Account'} />
   );
 };
 
